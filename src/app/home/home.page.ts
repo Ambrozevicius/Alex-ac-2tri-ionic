6dab1fca7bfbcdf3f49d7a499ea436a189687c2d
Isaac <ipambrozevicius@gmail.com>
2025-05-14 09:31:56 -0300

    feito quase tudo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,28 +10,36 @@ export class HomePage {
 
   constructor() {}
 
-  mes1 = 0
-  mes2 = 0
-  mes3 = 0
-  media = 0
+  mes1 = 0;
+  mes2 = 0;
+  mes3 = 0;
+  mes4 = 0;
+  media = 0;
 
-  abacaxi = false
-  maca = false
-  uva = false
-  banana = false
-  melancia = false
-
+  maca = false;
+  banana = false;
+  pessego = false;
+  manga = false;
+  abacaxi = false;
+  melancia = false;
+  limao = false;
+  morango = false;
 
   mediameses() {
-    this.media = (this.mes1 + this.mes2 + this.mes3) / 3
+    this.media = (this.mes1 + this.mes2 + this.mes3 + this.mes4) / 4;
 
-    if (this.media <= 20) {
-      this.abacaxi = false
-      this.maca = true
-      this.uva = false
-      this.banana = false
-      this.melancia = true
+    this.maca = this.media >= 15 && this.media <= 22;
+    this.banana = this.media >= 20 && this.media <= 35;
+    this.pessego = this.media >= 18 && this.media <= 25;
+    this.manga = this.media >= 20 && this.media <= 30;
+    this.abacaxi = this.media >= 20 && this.media <= 30;
+    this.melancia = this.media >= 25 && this.media <= 35;
+    this.limao = this.media >= 15 && this.media <= 30;
+    this.morango = this.media >= 15 && this.media <= 25;
+
+    if (this.maca && this.banana && this.pessego && this.manga && this.abacaxi && this.melancia && this.limao && this.morango) {
+      alert("Infelizmente, nenhuma fruta é compatível com esta temperatura 😖");
     }
-console.log(this.melancia, this.melancia, this.media)
+    console.log(this.maca,this.banana, this.pessego, this.manga, this.abacaxi, this.melancia, this.limao, this.morango)
   }
 }
