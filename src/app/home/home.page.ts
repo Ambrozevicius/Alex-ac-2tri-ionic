a7f8e518ad3922d627aace583908ef3f49f819cc
Isaac <ipambrozevicius@gmail.com>
2025-05-14 08:49:00 -0300

    adicionei a lista das frutas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,4 +10,28 @@ export class HomePage {
 
   constructor() {}
 
+  mes1 = 0
+  mes2 = 0
+  mes3 = 0
+  media = 0
+
+  abacaxi = false
+  maca = false
+  uva = false
+  banana = false
+  melancia = false
+
+
+  mediameses() {
+    this.media = (this.mes1 + this.mes2 + this.mes3) / 3
+
+    if (this.media <= 20) {
+      this.abacaxi = false
+      this.maca = true
+      this.uva = false
+      this.banana = false
+      this.melancia = true
+    }
+console.log(this.melancia, this.melancia, this.media)
+  }
 }
